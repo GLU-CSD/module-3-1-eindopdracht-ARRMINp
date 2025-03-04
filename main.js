8557526c93a042fb1cc5d4ab7edf2962de9dcc0e
ARRMINp <arminpost@icloud.com>
2025-03-04 09:51:40 +0100

    bestelformulier semi klaar

--- main.js
+++ main.js
@@ -4,4 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+        function validateForm() {
+            var form = document.getElementById('myForm');
+            if (!form.checkValidity()) {
+                // Als het formulier niet geldig is, voorkom verzending en geef feedback
+                alert('Vul alstublieft alle verplichte velden in.');
+                return false;
+            }
+            // Als het formulier geldig is, kan het verzonden worden
+            return true;
+        }
 
+        
